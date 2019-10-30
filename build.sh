ab40f267486e9cc50434d6bec0cd788634c35469
Robert Dunne <robe_dunne@hotmail.com>
2019-10-30 11:31:46 +0000

    added ci

---
+++ build.sh
@@ -0,0 +1,11 @@
+set -ev
+docker run -it --rm --privileged --name hassio-telegraf \
+        -v ~/.docker:/root/.docker \
+	-v "$(pwd)":/docker \
+	hassioaddons/build-env:latest \
+	--target hassio-telegraf \
+	--git \
+	--all \
+	--from "homeassistant/{arch}-base \
+	--author "Robert Dunne <robe_dunne@hotmail.com" \
+echo "Local Docker Build Successful"
