25aa29e300ee7c1247f9f09a5b9c3c7268a8b50b
Robert Dunne <robe_dunne@hotmail.com>
2019-10-30 12:09:40 +0000

    trying another fix

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -ev
 docker run -it --rm --privileged --name hassio-telegraf \
         -v ~/.docker:/root/.docker \
-	-v "$(pwd)":/docker \
+	-v "$(pwd)"/telegraf:/docker \
 	hassioaddons/build-env:latest \
 	--target hassio-telegraf \
 	--git \
