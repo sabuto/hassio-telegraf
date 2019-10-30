9dba89d97c8870fcec95f7f79b08f8799a73c80c
Robert Dunne <robe_dunne@hotmail.com>
2019-10-30 12:43:15 +0000

    add dist

---
+++ dist.sh
@@ -0,0 +1,22 @@
+set -ev
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo "This build is a pull request, abort!"
+    exit 0
+fi
+
+docker run -it --rm --privileged --name hassio-telegraf \
+        -v ~/.docker:/root/.docker \
+	-v "$(pwd)":/docker \
+	hassioaddons/build-env:latest \
+	--target telegraf \
+	--tag-latest
+	--push \
+	--all \
+	--image "sabuto/{arch}-hassio-telegraf" \
+	--from "homeassistant/{arch}-base" \
+	--author "Robert Dunne <robe_dunne@hotmail.com"
+	--doc-url "${GITHUB_URL}" \
+	--login "${DOCKER_USERNAME}" \
+	--password "${DOCKER_PASSWORD}" \
+	--parallel
