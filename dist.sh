fca10800b842f1d2fea83bd972ea7debc2d708eb
Robert Dunne <robe_dunne@hotmail.com>
2019-10-31 11:03:40 +0000

    added dist

--- dist.sh
+++ dist.sh
@@ -8,7 +8,7 @@ fi
 docker pull homeassistant/amd64-builder
 
 docker run --rm --privileged \
-	-v /var/run/docker.sock:/var/run/docker.sock
+	-v /var/run/docker.sock:/var/run/docker.sock \
         -v ~/.docker:/root/.docker \
 	-v "$(pwd)":/data \
 	homeassistant/amd64-builder \
