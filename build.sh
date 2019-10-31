16ee47aa83c4c849d7c092be514ec0e94aa14a30
Robert Dunne <robe_dunne@hotmail.com>
2019-10-31 10:43:08 +0000

    testing build

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@ set -ev
 docker pull homeassistant/amd64-builder
 
 docker run --rm --privileged \
+	-v /var/run/docker.sock:/var/run/docker.sock \
         -v ~/.docker:/root/.docker \
 	-v "$(pwd)":/data \
 	homeassistant/amd64-builder \
 	-t telegraf \
-	--all 
+	--all \
+	--test
